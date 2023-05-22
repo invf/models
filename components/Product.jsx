effbbf5eedb853eb42452f1461a22db1cceed218
invf <115990356+invf@users.noreply.github.com>
2023-05-22 16:58:44 +0300

    add GA4 to Product.js

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
-
 import { urlFor } from '../lib/client';
+import ReactGA from 'react-ga4';
 
 const Product = ({ product: { image, name, slug, price } }) => {
+  const TRACKING_ID = 'G-3MGRZ05J9J';
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.send({ hitType: 'pageview', page: `/product/${slug.current}`, title: name });
+  }, [name, slug.current]);
+
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className='product-card'>
-          <img 
+          <img
             src={urlFor(image && image[0])}
-            wigth={330}
+            width={330}
             height={330}
-            className="product-image"
+            className='product-image'
+            alt={name}
           />
           <p className='product-name'>{name}</p>
           <p className='product-price'>${price}</p>
         </div>
       </Link>
     </div>
+  );
+};
 
-  
-
-  )
-}
-
-export default Product
+export default Product;
