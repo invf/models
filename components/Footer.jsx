ec91b7de06bdb2343861dc30ab59f54fc9d87a91
invf <115990356+invf@users.noreply.github.com>
2023-05-23 11:19:22 +0300

    add Social Link in Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+import { SocialIcon } from 'react-social-icons';
+
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2023 Handstitches. All rights reserverd</p>
-      <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
-      </p>
+      
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <SocialIcon url="https://pinterest.com/handstitchesua" style={{ margin: '0 5px' }} />
+      <SocialIcon url="https://reddit.com/" style={{ margin: '0 5px' }} />
+      <SocialIcon url="https://twitter.com/" style={{ margin: '0 5px' }} />
     </div>
+      
+    </div>
+
+    
   )
+  
 }
+
+
 
 export default Footer
